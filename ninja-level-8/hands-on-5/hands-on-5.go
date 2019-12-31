00f28ee57aead4294235e14764f52e4e9fe93efa
Carlos Eduardo Witte <cewitte@outlook.com>
2019-12-31 18:21:20 -0300

    Add hands-on-5.go successfully running

---
+++ ninja-level-8/hands-on-5/hands-on-5.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type user struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
+// ByAge implements sort.Interface for []Person based on
+// the Age field.
+type ByAge []user
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+// ByAge implements sort.Interface for []Person based on
+// the Age field.
+type ByLast []user
+
+func (a ByLast) Len() int           { return len(a) }
+func (a ByLast) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByLast) Less(i, j int) bool { return a[i].Last < a[j].Last }
+
+func friendlyPrint(users *[]user) {
+	for _, v := range *users {
+		fmt.Printf("My name is %v %v and I am %v years old. Here are some of my famous quotes:\n", v.First, v.Last, v.Age)
+		for _, s := range v.Sayings {
+			fmt.Println("\t", s)
+		}
+	}
+}
+
+func main() {
+	u1 := user{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	u2 := user{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	u3 := user{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+
+	// your code goes here
+	sort.Sort(ByAge(users))
+	fmt.Println("\nSorted by age:")
+	friendlyPrint(&users)
+
+	sort.Sort(ByLast(users))
+	fmt.Println("\nSorted by last:")
+	friendlyPrint(&users)
+
+}
