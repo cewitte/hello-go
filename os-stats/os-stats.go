44de1a6df2bfea7e0418bf23d25fdbeeb302729a
Carlos Eduardo Witte <cewitte@outlook.com>
2019-12-31 19:26:59 -0300

    Add os stats and concurrency experiment

---
+++ os-stats/os-stats.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	fmt.Println("OS Arch:\t\t", runtime.GOOS)
+	fmt.Println("Number of CPUs:\t\t", runtime.NumCPU())
+
+	wg.Add(8)
+	for i := 1; i <= 8; i++ {
+		go foo(0, i)
+	}
+	wg.Wait()
+}
+
+func foo(s, e int) {
+	for i := s; i <= e; i++ {
+		fmt.Println(math.Pow(2, float64(i)))
+	}
+
+	wg.Done()
+}
