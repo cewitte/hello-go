da65058e735da6bb56133d35b48e5aff2314f956
Carlos Eduardo Witte <cewitte@outlook.com>
2020-01-07 17:58:09 -0300

    Add fix to ninja-level-12 exercises

--- ninja-level-12/hands-on-1/main.go
+++ ninja-level-12/hands-on-1/main.go
@@ -1,14 +1,24 @@
 package main
 
-type canine struct{
+import (
+	"fmt"
+	"github.com/cewitte/hello-go/ninja-level-12/hands-on-1/dog"
+)
+
+type canine struct {
 	name string
-	age int
+	age  int
 }
 
-
-func main(){
+func main() {
 	// dogYears := Years(7)
 	// fmt.Printf("Human's age in dog years is %v", dogYears)
 
-	
+	fido := canine{
+		name: "Fido",
+		age:  dog.Years(7),
+	}
+
+	fmt.Println(fido)
+
 }
