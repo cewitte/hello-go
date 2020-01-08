29ee9c97b564cff77c29f6dbb6174e715a993904
Carlos Eduardo Witte <cewitte@outlook.com>
2020-01-08 09:53:29 -0300

    Add testing code with table tests, this time it's working fine

--- go-testing/testing.go
+++ go-testing/testing.go
@@ -1,5 +1,7 @@
 // Package mathOps performs math operations
-package mathOps
+package main
+
+import "fmt"
 
 // average returns the average of a slice of integers
 func average(x ...int) int {
@@ -13,9 +15,9 @@ func average(x ...int) int {
 	return sum / a
 }
 
-// func main() {
+func main() {
 
-// 	fmt.Println("The average of 2, 4 and 6 is...", average(2, 4, 6))
-// 	fmt.Println("The average of 3, 5 is...", average(3, 5))
-// 	fmt.Println("The average of 12, 24, 42, 54 and 36 is...", average(12, 24, 42, 54, 36))
-// }
+	fmt.Println("The average of 87, 53, 87, 99, 13...", average(87, 53, 87, 99, 13))
+	// fmt.Println("The average of 3, 5 is...", average(3, 5))
+	// fmt.Println("The average of 12, 24, 42, 54 and 36 is...", average(12, 24, 42, 54, 36))
+}
--- go-testing/testing_test.go
+++ go-testing/testing_test.go
@@ -9,10 +9,10 @@ type test struct {
 
 func TestAverage(t *testing.T) {
 	tests := []test{
-		test{[]int{87, 53, 87, 99, 13}, 87},
-		test{[]int{85, 29, 93, 14, 48}, 48},
-		test{[]int{31, 51, 34, 66, 58}, 51},
-		test{[]int{50, 38, 62, 72, 81}, 62},
+		test{[]int{87, 53, 87, 99, 13}, 67},
+		test{[]int{85, 29, 93, 14, 48}, 53},
+		test{[]int{31, 51, 34, 66, 58}, 48},
+		test{[]int{50, 38, 62, 72, 81}, 60},
 	}
 
 	for _, v := range tests {
@@ -21,8 +21,4 @@ func TestAverage(t *testing.T) {
 			t.Error("Expected", v.answer, "Got", x)
 		}
 	}
-}
-
-func main() {
-
 }
